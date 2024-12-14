24341f77040184d05b6784cfedbc81e6068f6466
Chris Pollard <cspollard@gmail.com>
2024-12-14 09:23:27 +0000

    organize

--- plotkin.py
+++ plotkin.py
@@ -41,7 +41,7 @@ def numpy2d(h):
 def numpy1d(h):
   values = \
     numpy.array \
-    ( [ h.bin(xidx).val() for xidx in range(1, h.numBinsX()+1) ]
+    ( [ b.val() for b in h.bins() ]
     )
 
   return numpy.array(h.xEdges()) , values / h.annotation("ScaledBy")
@@ -52,9 +52,15 @@ def double(xs, ys):
 
 
 def gethists(yodadict):
-  sumW = yodadict["/RAW/_EVTCOUNT"].sumW()
-  pTl = numpy1d(yodadict["/MyAnalysis/hist_e_pT_reconstructed"])
-  mT = numpy1d(yodadict["/MyAnalysis/hist_mT_reconstructed"])
+  evtcount = yodadict["/RAW/_EVTCOUNT"]
+  sumW = evtcount.sumW()
+
+  hpTl = yodadict["/MyAnalysis/hist_e_pT_reconstructed"]
+  hmT = yodadict["/MyAnalysis/hist_mT_reconstructed"]
+
+  pTl = numpy1d(hpTl)
+  mT = numpy1d(hmT)
+
   pTl = (pTl[0] , pTl[1] / sumW)
   mT = (mT[0] , mT[1] / sumW)
   return { "pTl" : pTl , "mT" : mT }
@@ -84,6 +90,8 @@ for k in ["pTl", "mT"]:
   _ , bsmplus = cdfbsmplus[k]
   _ , bsmminus = cdfbsmminus[k]
 
+  widths = edges[1:] - edges[:-1]
+
   smpred = \
       smplus * xsecs["CDF"]["SM+"] \
     + smminus * xsecs["CDF"]["SM-"]
@@ -95,26 +103,76 @@ for k in ["pTl", "mT"]:
   smpred = smpred * lumiCDF
   bsmpred = bsmpred * lumiCDF
 
-  print("CDF total SM prediction: %.2e events" % smpred.sum())
-  print("CDF total BSM prediction: %.2e events" % bsmpred.sum())
+  print("CDF total SM prediction: %.2e events" % (smpred * widths).sum())
+  print("CDF total BSM prediction: %.2e events" % (bsmpred * widths).sum())
 
   xs , sm = h2curve((edges, smpred))
   _ , bsm = h2curve((edges, bsmpred))
 
-  fig = Figure((6, 6))
+
+  fig = Figure((5, 5))
   plt = fig.add_subplot(3, 1, (1, 2))
 
-  plt.plot(xs, sm, color="blue", label="SM")
-  plt.plot(xs, bsm, color="orange", label="BSM")
+  plt.plot(xs, sm, color="blue", label=r"$W \to \ell \nu$", lw=2)
+  plt.plot(xs, bsm, color="orange", label=r"$W \to \ell N$ ", lw=2, ls=":")
   plt.set_xticks([])
-  plt.set_ylabel("events / bin")
+  plt.set_ylabel("events / GeV")
 
   ratioplt = fig.add_subplot(3, 1, 3)
-  ratioplt.plot(xs, bsm/sm, color="orange")
+  ratioplt.plot(xs, bsm/sm, color="orange", lw=2, ls=":")
   ratioplt.set_xlabel(axisdict[k])
   ratioplt.set_ylabel("ratio")
 
-  plt.legend(title="CDF\n$W^+$ and $W^-$\nelectron channel")
+  plt.legend(title="CDF electron channel")
 
   fig.tight_layout()
   fig.savefig("CDF-%s.png" % k)
+  fig.savefig("CDF-%s.pdf" % k)
+  fig.clf()
+
+
+for k in ["pTl", "mT"]:
+  edges , smplus = atlassmplus[k]
+  _ , smminus = atlassmplus[k]
+  _ , bsmplus = atlasbsmplus[k]
+  _ , bsmminus = atlasbsmminus[k]
+
+  widths = edges[1:] - edges[:-1]
+
+  smpredplus = smplus * xsecs["ATLAS"]["SM+"] * lumiATLAS
+  smpredminus = smminus * xsecs["ATLAS"]["SM-"] * lumiATLAS
+
+  bsmpredplus = bsmplus * xsecs["ATLAS"]["BSM+"] * lumiATLAS
+  bsmpredminus = bsmminus * xsecs["ATLAS"]["BSM-"] * lumiATLAS
+
+  print("ATLAS total SM+ prediction: %.2e events" % (smpredplus * widths).sum())
+  print("ATLAS total SM- prediction: %.2e events" % (smpredminus * widths).sum())
+  print("ATLAS total BSM+ prediction: %.2e events" % (smpredplus * widths).sum())
+  print("ATLAS total BSM- prediction: %.2e events" % (smpredminus * widths).sum())
+
+  xs , smplus = h2curve((edges, smpredplus))
+  _ , smminus = h2curve((edges, smpredminus))
+  _ , bsmplus = h2curve((edges, bsmpredplus))
+  _ , bsmminus = h2curve((edges, bsmpredminus))
+
+  plt = fig.add_subplot(3, 1, (1, 2))
+
+  plt.plot(xs, smplus, color="blue", label=r"$W^+ \to \ell^+ \nu_\ell$", lw=2)
+  plt.plot(xs, smminus, color="orange", label=r"$W^- \to \ell^- \bar \nu_\ell$", lw=2)
+  plt.plot(xs, bsmplus, color="blue", label=r"$W^+ \to \ell^+ N$", lw=2, ls=":")
+  plt.plot(xs, bsmminus, color="orange", label=r"$W^- \to \ell^- \bar N$", lw=2, ls=":")
+  plt.set_xticks([])
+  plt.set_ylabel("events / GeV")
+
+  ratioplt = fig.add_subplot(3, 1, 3)
+  ratioplt.plot(xs, bsmplus/smplus, color="blue", lw=2, ls=":")
+  ratioplt.plot(xs, bsmminus/smminus, color="orange", lw=2, ls=":")
+  ratioplt.set_xlabel(axisdict[k])
+  ratioplt.set_ylabel("ratio")
+
+  plt.legend(title="ATLAS electron channel")
+
+  fig.tight_layout()
+  fig.savefig("ATLAS-%s.png" % k)
+  fig.savefig("ATLAS-%s.pdf" % k)
+  fig.clf()
