569f09550ca46bb6d56b2805beb3fd9e0d4638de
Chris Pollard <cspollard@gmail.com>
2025-02-10 16:54:32 +0000

    yeah

--- plotkin.py
+++ plotkin.py
@@ -2,28 +2,6 @@ import yoda
 import numpy
 from matplotlib.figure import Figure
 
-lumiCDF = 8800
-lumiATLAS = 4700
-
-xsecs = \
-  { "CDF" :
-    { "SM+" : 866.4
-    , "SM-" : 866.4
-    , "BSM+" : 202
-    , "BSM-" : 202
-    }
-  , "ATLAS" :
-    { "SM+" : 3464
-    , "SM-" : 1909
-    , "BSM+" : 610.1
-    , "BSM-" : 615.6
-    }
-  }
-
-axisdict = \
-  { "pTl" : r"$p_T^\ell$ [GeV]"
-  , "mT" : r"$m_T$ [GeV]"
-  }
 
 def numpy2d(h):
   values = \
@@ -34,7 +12,6 @@ def numpy2d(h):
         for xidx in range(1, h.numBinsX()+1)
       ]
     )
-
   return numpy.array(h.xEdges()) , values / h.annotation("ScaledBy")
 
 
@@ -43,7 +20,6 @@ def numpy1d(h):
     numpy.array \
     ( [ b.val() for b in h.bins() ]
     )
-
   return numpy.array(h.xEdges()) , values / h.annotation("ScaledBy")
 
 
@@ -72,107 +48,137 @@ def h2curve(h):
 
   return xs, ys
 
+# lumi in pb
+lumi = \
+  { "CDF" : 8800
+  , "ATLAS" : 4700
+  , "CMS" : 16800
+  }
 
-cdfsmplus = gethists(yoda.read("yoda/ppeplusvestable_cdf/Rivet_merged.yoda"))
-cdfsmminus = gethists(yoda.read("yoda/ppeminusvestable_cdf/Rivet_merged.yoda"))
-cdfbsmplus = gethists(yoda.read("yoda/ppeplusn1stable_cdf/Rivet_merged.yoda"))
-cdfbsmminus = gethists(yoda.read("yoda/ppeminusn1stable_cdf/Rivet_merged.yoda"))
+# all in pb
+xsecs = \
+  { "CDF" :
+    { "SM+" : 866.4
+    , "SM-" : 866.4
+    , "BSM+" : 202
+    , "BSM-" : 202
+    }
 
-atlassmplus = gethists(yoda.read("yoda/ppeplusvestable/Rivet_merged.yoda"))
-atlassmminus = gethists(yoda.read("yoda/ppeminusvestable/Rivet_merged.yoda"))
-atlasbsmplus = gethists(yoda.read("yoda/ppeplusn1stable/Rivet_merged.yoda"))
-atlasbsmminus = gethists(yoda.read("yoda/ppeminusn1stable/Rivet_merged.yoda"))
+  , "ATLAS" :
+    { "SM+" : 3464
+    , "SM-" : 1909
+    , "BSM+" : 610.1
+    , "BSM-" : 615.6
+    }
 
+  , "CMS" :
+    { "SM+" : 5666
+    , "SM-" : 3601
+    , "BSM+" : 1053
+    , "BSM-" : 1123
+    }
+  }
 
-for k in ["pTl", "mT"]:
-  edges , smplus = cdfsmplus[k]
-  _ , smminus = cdfsmplus[k]
-  _ , bsmplus = cdfbsmplus[k]
-  _ , bsmminus = cdfbsmminus[k]
+procs = \
+  { "CDF" :
+    { "SM+" : gethists(yoda.read("yoda/ppeplusvestable_cdf/Rivet_merged.yoda"))
+    , "SM-" : gethists(yoda.read("yoda/ppeminusvestable_cdf/Rivet_merged.yoda"))
+    , "BSM+" : gethists(yoda.read("yoda/ppeplusn1stable_cdf/Rivet_merged.yoda"))
+    , "BSM-" : gethists(yoda.read("yoda/ppeminusn1stable_cdf/Rivet_merged.yoda"))
+    }
 
-  widths = edges[1:] - edges[:-1]
+  , "ATLAS" :
+    { "SM+" : gethists(yoda.read("yoda/ppeplusvestable/Rivet_merged.yoda"))
+    , "SM-" : gethists(yoda.read("yoda/ppeminusvestable/Rivet_merged.yoda"))
+    , "BSM+" : gethists(yoda.read("yoda/ppeplusn1stable/Rivet_merged.yoda"))
+    , "BSM-" : gethists(yoda.read("yoda/ppeminusn1stable/Rivet_merged.yoda"))
+    }
+  
+  , "CMS" :
+    { "SM+" : gethists(yoda.read("yoda/Rivet_SMW+_CMS.yoda"))
+    , "SM-" : gethists(yoda.read("yoda/Rivet_SMW-_CMS.yoda"))
+    , "BSM+" : gethists(yoda.read("yoda/Rivet_BSMW+_CMS.yoda"))
+    , "BSM-" : gethists(yoda.read("yoda/Rivet_BSMW-_CMS.yoda"))
+    }
+  }
 
-  smpred = \
-      smplus * xsecs["CDF"]["SM+"] \
-    + smminus * xsecs["CDF"]["SM-"]
-
-  bsmpred = \
-      bsmplus * xsecs["CDF"]["BSM+"] \
-    + bsmminus * xsecs["CDF"]["BSM-"]
-
-  smpred = smpred * lumiCDF
-  bsmpred = bsmpred * lumiCDF
-
-  print("CDF total SM prediction: %.2e events" % (smpred * widths).sum())
-  print("CDF total BSM prediction: %.2e events" % (bsmpred * widths).sum())
-
-  xs , sm = h2curve((edges, smpred))
-  _ , bsm = h2curve((edges, bsmpred))
-
-
-  fig = Figure((5, 5))
-  plt = fig.add_subplot(3, 1, (1, 2))
-
-  plt.plot(xs, sm, color="blue", label=r"$W \to \ell \nu$", lw=2)
-  plt.plot(xs, bsm, color="orange", label=r"$W \to \ell N$ ", lw=2, ls=":")
-  plt.set_xticks([])
-  plt.set_ylabel("events / GeV")
-
-  ratioplt = fig.add_subplot(3, 1, 3)
-  ratioplt.plot(xs, bsm/sm, color="orange", lw=2, ls=":")
-  ratioplt.set_xlabel(axisdict[k])
-  ratioplt.set_ylabel("ratio")
-
-  plt.legend(title="CDF electron channel")
-
-  fig.tight_layout()
-  fig.savefig("CDF-%s.png" % k)
-  fig.savefig("CDF-%s.pdf" % k)
-  fig.clf()
+axisdict = \
+  { "pTl" : r"$p_T^\ell$ [GeV]"
+  , "mT" : r"$m_T$ [GeV]"
+  }
 
 
-for k in ["pTl", "mT"]:
-  edges , smplus = atlassmplus[k]
-  _ , smminus = atlassmplus[k]
-  _ , bsmplus = atlasbsmplus[k]
-  _ , bsmminus = atlasbsmminus[k]
+fig = Figure((5, 5))
+for experiment in ["CMS" , "CDF" , "ATLAS"]:
+  for kin in ["pTl", "mT"]:
+    edges , smplus = procs[experiment]["SM+"][kin]
+    _ , smminus = procs[experiment]["SM-"][kin]
+    _ , bsmplus = procs[experiment]["BSM+"][kin]
+    _ , bsmminus = procs[experiment]["BSM-"][kin]
 
-  widths = edges[1:] - edges[:-1]
+    widths = edges[1:] - edges[:-1]
 
-  smpredplus = smplus * xsecs["ATLAS"]["SM+"] * lumiATLAS
-  smpredminus = smminus * xsecs["ATLAS"]["SM-"] * lumiATLAS
+    smpredplus = smplus * xsecs[experiment]["SM+"] * lumi[experiment]
+    smpredminus = smminus * xsecs[experiment]["SM-"] * lumi[experiment]
+    smpred = smpredplus + smpredminus
 
-  bsmpredplus = bsmplus * xsecs["ATLAS"]["BSM+"] * lumiATLAS
-  bsmpredminus = bsmminus * xsecs["ATLAS"]["BSM-"] * lumiATLAS
+    bsmpredplus = bsmplus * xsecs[experiment]["BSM+"] * lumi[experiment]
+    bsmpredminus = bsmminus * xsecs[experiment]["BSM-"] * lumi[experiment]
+    bsmpred = bsmpredplus + bsmpredminus
 
-  print("ATLAS total SM+ prediction: %.2e events" % (smpredplus * widths).sum())
-  print("ATLAS total SM- prediction: %.2e events" % (smpredminus * widths).sum())
-  print("ATLAS total BSM+ prediction: %.2e events" % (smpredplus * widths).sum())
-  print("ATLAS total BSM- prediction: %.2e events" % (smpredminus * widths).sum())
+    print("%s total SM+ prediction: %.2e events" % (experiment , (smpredplus * widths).sum()))
+    print("%s total SM- prediction: %.2e events" % (experiment , (smpredminus * widths).sum()))
+    print("%s total BSM+ prediction: %.2e events" % (experiment , (smpredplus * widths).sum()))
+    print("%s total BSM- prediction: %.2e events" % (experiment , (smpredminus * widths).sum()))
+    print("%s total SM prediction: %.2e events" % (experiment , (smpred * widths).sum()))
+    print("%s total BSM prediction: %.2e events" % (experiment , (bsmpred * widths).sum()))
 
-  xs , smplus = h2curve((edges, smpredplus))
-  _ , smminus = h2curve((edges, smpredminus))
-  _ , bsmplus = h2curve((edges, bsmpredplus))
-  _ , bsmminus = h2curve((edges, bsmpredminus))
+    xs , sm = h2curve((edges, smpred))
+    _ , bsm = h2curve((edges, bsmpred))
 
-  plt = fig.add_subplot(3, 1, (1, 2))
+    plt = fig.add_subplot(3, 1, (1, 2))
 
-  plt.plot(xs, smplus, color="blue", label=r"$W^+ \to \ell^+ \nu_\ell$", lw=2)
-  plt.plot(xs, smminus, color="orange", label=r"$W^- \to \ell^- \bar \nu_\ell$", lw=2)
-  plt.plot(xs, bsmplus, color="blue", label=r"$W^+ \to \ell^+ N$", lw=2, ls=":")
-  plt.plot(xs, bsmminus, color="orange", label=r"$W^- \to \ell^- \bar N$", lw=2, ls=":")
-  plt.set_xticks([])
-  plt.set_ylabel("events / GeV")
+    plt.plot(xs, sm, color="blue", label=r"$W \to \ell \nu$", lw=2)
+    plt.plot(xs, bsm, color="orange", label=r"$W \to \ell N$ ", lw=2, ls=":")
+    plt.set_xticks([])
+    plt.set_ylabel("events / GeV")
 
-  ratioplt = fig.add_subplot(3, 1, 3)
-  ratioplt.plot(xs, bsmplus/smplus, color="blue", lw=2, ls=":")
-  ratioplt.plot(xs, bsmminus/smminus, color="orange", lw=2, ls=":")
-  ratioplt.set_xlabel(axisdict[k])
-  ratioplt.set_ylabel("ratio")
+    ratioplt = fig.add_subplot(3, 1, 3)
+    ratioplt.plot(xs, bsm/sm, color="orange", lw=2, ls=":")
+    ratioplt.set_xlabel(axisdict[kin])
+    ratioplt.set_ylabel("ratio")
 
-  plt.legend(title="ATLAS electron channel")
+    plt.legend(title="%s electron channel" % experiment)
 
-  fig.tight_layout()
-  fig.savefig("ATLAS-%s.png" % k)
-  fig.savefig("ATLAS-%s.pdf" % k)
-  fig.clf()
+    fig.tight_layout()
+    fig.savefig("%s-%s-total.png" % (experiment , kin))
+    fig.savefig("%s-%s-total.pdf" % (experiment , kin))
+    fig.clf()
+
+
+    xs , smplus = h2curve((edges, smpredplus))
+    _ , smminus = h2curve((edges, smpredminus))
+    _ , bsmplus = h2curve((edges, bsmpredplus))
+    _ , bsmminus = h2curve((edges, bsmpredminus))
+
+    plt = fig.add_subplot(3, 1, (1, 2))
+
+    plt.plot(xs, smplus, color="blue", label=r"$W^+ \to \ell^+ \nu_\ell$", lw=2)
+    plt.plot(xs, smminus, color="orange", label=r"$W^- \to \ell^- \bar \nu_\ell$", lw=2)
+    plt.plot(xs, bsmplus, color="blue", label=r"$W^+ \to \ell^+ N$", lw=2, ls=":")
+    plt.plot(xs, bsmminus, color="orange", label=r"$W^- \to \ell^- \bar N$", lw=2, ls=":")
+    plt.set_xticks([])
+    plt.set_ylabel("events / GeV")
+
+    ratioplt = fig.add_subplot(3, 1, 3)
+    ratioplt.plot(xs, bsmplus/smplus, color="blue", lw=2, ls=":")
+    ratioplt.plot(xs, bsmminus/smminus, color="orange", lw=2, ls=":")
+    ratioplt.set_xlabel(axisdict[kin])
+    ratioplt.set_ylabel("ratio")
+
+    plt.legend(title="%s electron channel" % experiment)
+
+    fig.tight_layout()
+    fig.savefig("%s-%s.png" % (experiment , kin))
+    fig.savefig("%s-%s.pdf" % (experiment , kin))
+    fig.clf()
